28e98def613c97f3234ac233a4e3754917ad4cd9
Mariana Oliveira <39959182+marianamsoliveira@users.noreply.github.com>
2026-01-03 18:51:10 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,78 @@
+import { pegaLivros, adicionaLivro, removeLivro, buscaLivrosPorAutor, listarLivrosAposAno, salvaLivrosArquivo } from "./livraria.js";
+import { menu, finaliza, le, textoAzul, textoVerde, menuSelecaoUsuario } from "./interface.js"
+
+
+const itensMenu = [" Adicionar ", " Editar ", " Remover ", " Ver todos ", " Busca por autor ", " Listar por ano ", " Salvar no arquivo ", " Sair "]
+
+let titulo, autor, ano, busca, lista, id
+
+while (1) {
+    const selecionado = await menu(itensMenu)
+    switch (selecionado.trim().toLocaleLowerCase()) {
+        case "adicionar":
+            titulo = await le("\nInforme o título do livro: ")
+            autor = await le("\nInforme o autor do livro: ")
+            ano = await le("\nInforme o ano de publicação: ")
+            adicionaLivro(titulo, autor, ano)
+            textoVerde("Dados inseridos com sucesso!\n")
+            break
+        case "editar":
+            id = await menuSelecaoUsuario(pegaLivros())
+            titulo = await le("\nDigite o título atualizado: ")
+            autor = await le("\nDigite o nome do autor atualizado: ")
+            ano = await le("\nDigite o ano de publicação atualizado: ")
+            adicionaLivro(titulo, autor, ano, id)
+            textoVerde("Dados atualizados com sucesso!\n")
+            break
+        case "remover":
+            id = await menuSelecaoUsuario(pegaLivros())
+            removeLivro(id)
+            textoVerde("Dados removidos com sucesso!\n")
+            break
+        case "ver todos":
+            busca = pegaLivros()
+            textoAzul(busca+"\n")
+            break
+        case "busca por autor":
+            autor = await le("\nInforme o autor do livro: ")
+            busca = buscaLivrosPorAutor(autor)
+            textoAzul(busca+"\n")
+            break
+        case "listar por ano":
+            ano = await le("\nInforme o ano de publicação: ")
+            lista = listarLivrosAposAno(ano)
+            textoAzul(lista+"\n")
+            break
+        case "salvar no arquivo":
+            salvaLivrosArquivo()
+            textoVerde("Dados salvos com sucesso!\n")
+            break
+        case "sair":
+            finaliza()
+            textoVerde("Até mais")
+        default:
+            console.log(`${selecionado.trim().toLocaleLowerCase()} não é válido.`)
+    }
+}
+
+
+
+
+// console.log(pegaLivros())
+
+// adicionaLivro('O empresário', 'Mariana Oliveira', 2025)
+// adicionaLivro('A cruz e a Espada', 'Oliveira Alves', 2014)
+
+// console.log(pegaLivros())
+
+// removeLivro('O empresário')
+
+// console.log(pegaLivros())
+
+// console.log(buscaLivrosPorAutor("Cecília Meireles"))
+
+// console.log(listarLivrosAposAno(1990))
+
+// console.log(ordenarLivrosPorTitulo())
+
+// salvaLivrosArquivo()
---
+++ livraria.js
@@ -0,0 +1,70 @@
+import fs from "fs"
+import { v4 as uuidv4 } from "uuid"
+
+let livros = _carregaLivros()
+
+function _carregaLivros() {
+    const dadosJSON = fs.readFileSync("livraria.json", "utf8")
+    const dados = JSON.parse(dadosJSON)
+    return dados
+}
+
+export function pegaLivros() {
+    const livrosImprimiveis = livros.map(livro => `\nId: ${livro.id}, Título: ${livro.titulo}, Autor: ${livro.autor}, Publicado em: ${livro.ano}`)
+    return livrosImprimiveis
+}
+
+export function listarLivrosAposAno(ano) {
+    if (ano) {
+        const livrosImprimiveis = livros.filter(livro => +livro.ano > +ano).map(livro => `\nId: ${livro.id}, Título: ${livro.titulo}, Autor: ${livro.autor}, Publicado em: ${livro.ano}`)
+        if (livrosImprimiveis.length > 0) {
+            return livrosImprimiveis
+        }
+        else {
+            return `Ano: ${ano}\nNenhum livro encontrado`
+        }
+    }
+    else {
+        return 'Ano não informado'
+    }
+}
+export function buscaLivrosPorAutor(autor) {
+    if (autor) {
+        let livrosEncontrados
+        livrosEncontrados = livros.filter(livro => livro.autor === autor).map(livro => `\nId: ${livro.id}, Título: ${livro.titulo}, Autor: ${livro.autor}, Publicado em: ${livro.ano}`)
+        if (livrosEncontrados.length > 0) {
+            return livrosEncontrados
+        } else {
+            return `Autor: ${autor}\nNenhum livro encontrado`
+        }
+    } else {
+        return "Autor não informado"
+    }
+}
+
+export function _ordenarLivrosPorTitulo(livros) {
+    return livros.sort((a, b) => a.titulo.localeCompare(b.titulo))
+}
+
+export function adicionaLivro(titulo, autor, ano, id) {
+    const indice = livros.findIndex(livro => livro.id === id)
+    if (indice >= 0) {
+        livros[indice].titulo = titulo
+        livros[indice].autor = autor
+        livros[indice].ano = ano
+    } else {
+        const idUnico = uuidv4()
+        livros.push({ id: idUnico, titulo, autor, ano })
+    }
+}
+
+
+export function removeLivro(id) {
+    const novoLivro = livros.filter(livro => livro.id != id)
+    livros = novoLivro
+}
+
+export function salvaLivrosArquivo() {
+    const dadosJSON = JSON.stringify(_ordenarLivrosPorTitulo(livros), null, 4)
+    fs.writeFileSync("livraria.json", dadosJSON, "utf8")
+}
